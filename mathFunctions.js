68908ca3f5d7419a8964bb44a060793b4405a0b4
jperales64 <jaimeperales@rocketmail.com>
2021-12-07 16:04:47 -0600

    implemented screen ui and math functions

--- mathFunctions.js
+++ mathFunctions.js
@@ -1,3 +1,28 @@
+const runningFunction = {
+    a: null,
+    b: null,
+    operation: null,
+    result: null
+};
+
+const numberButtons = document.querySelectorAll('.number');
+
+const operationButtons = document.querySelectorAll('.operator');
+
+const screen = document.querySelector('.screen');
+
+const ac = document.getElementById('ac');
+
+const addButton = document.getElementById('add');
+
+const resultButton = document.getElementById('=');
+
+let numberLock = true;
+
+let resultRequested = false;
+
+const MAX_SCREEN_LENTH = 7;
+
 const add = function(a, b) {
     return a + b;
 }
@@ -11,7 +36,8 @@ const multiply = function(a, b) {
 }
 
 const divide = function(a, b) {
-    return a / b;
+    return Number((Math.round((a / b) * 100) / 100).toFixed(2));
+
 }
 
 const operate = function(operator, a, b) {
@@ -25,28 +51,147 @@ const operate = function(operator, a, b) {
         case 'divide':
             return divide(a, b);
         default:
-            console.alert("operate function was passed incorrect value")
+            alert("operate function was passed incorrect value: " + operator);
+            console.table(runningFunction);
     }
 }
 
-//Create a onclick function for numbers that displays number on screen
-//for now clear screen on each click!
+const clearRunningFunction = function() {
+    Object.keys(runningFunction).forEach((i) => runningFunction[i] = null)
+}
 
-const numberButtons = document.querySelectorAll('.number');
-
-const screen = document.querySelector('.screen');
-
-const ac = document.getElementById('ac');
-
-//more needs to be added to this ie clear number variables
-ac.addEventListener('click', () => {
+const clear = function() {
     screen.textContent = 0
-})
+    clearRunningFunction();
+    resultRequested = false;
+}
+
+const freshRun = function() {
+    return (runningFunction.a == null && runningFunction.b == null && runningFunction.operation == null && runningFunction.result == null) ? true : false;
+}
+
+const operationAndASet = function() {
+    return (runningFunction.a != null && runningFunction.b == null && runningFunction.operation != null && runningFunction.result == null) ? true : false;
+}
+
+const additionalOperation = function() {
+    return (runningFunction.a != null && runningFunction.b != null && runningFunction.operation != null && runningFunction.result != null && !resultRequested) ? true : false;
+}
+
+const additionalOperationAfterResult = function() {
+    return (runningFunction.a != null && runningFunction.b != null && runningFunction.operation != null && runningFunction.result != null && resultRequested) ? true : false;
+}
+
+const runFreshRun = function(operationButton) {
+    runningFunction.a = Number(screen.textContent);
+    runningFunction.operation = operationButton.id;
+    resultRequested = false;
+    numberLock = true;
+}
+
+const runOperationAndAset = function(operationButton) {
+    runningFunction.b = Number(screen.textContent);
+    runningFunction.result = operate(runningFunction.operation, runningFunction.a, runningFunction.b);
+    runningFunction.a = runningFunction.result;
+    resultRequested = false;
+    screen.textContent = runningFunction.result;
+    runningFunction.operation = operationButton.id;
+    numberLock = true;
+}
+
+const runAdditionalOperation = function(operationButton) {
+    runningFunction.b = Number(screen.textContent);
+    runningFunction.a = runningFunction.result;
+    runningFunction.result = operate(runningFunction.operation, runningFunction.a, runningFunction.b);
+    runningFunction.a = runningFunction.result;
+    runningFunction.operation = operationButton.id;
+    resultRequested = false;
+    screen.textContent = runningFunction.result;
+    numberLock = true;
+}
+
+const runAdditionalOperationAfterResult = function(operationButton) {
+    runningFunction.a = runningFunction.result;
+    runningFunction.operation = operationButton.id;
+    resultRequested = false;
+}
+
+const onOperatorButtonClick = function() {
+    if (freshRun()) {
+        runFreshRun(this);
+
+    } else if (operationAndASet()) {
+        runOperationAndAset(this);
+
+    } else if (additionalOperation()) {
+        runAdditionalOperation(this);
+
+    } else if (additionalOperationAfterResult()) {
+        runAdditionalOperationAfterResult(this);
+
+    } else {
+        console.log("unHandled case for add button");
+        console.table(runningFunction);
+    }
+}
 
 
 
-numberButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        screen.textContent = button.id;
+
+
+const runResultWithRequest = function() {
+    runningFunction.a = runningFunction.result;
+    runningFunction.result = operate(runningFunction.operation, runningFunction.a, runningFunction.b);
+    screen.textContent = runningFunction.result;
+    numberLock = true;
+}
+
+const runResultWithoutRequest = function() {
+    resultRequested = true;
+    runningFunction.b = Number(screen.textContent);
+    runningFunction.result = operate(runningFunction.operation, runningFunction.a, runningFunction.b);
+    screen.textContent = runningFunction.result;
+    numberLock = true;
+}
+
+const runResult = function() {
+    if (freshRun()) {
+
+    } else if (resultRequested) {
+        runResultWithRequest();
+
+    } else {
+        runResultWithoutRequest();
+    }
+}
+
+const displayText = function() {
+    if (resultRequested) {
+        clear();
+    }
+    if (screen.textContent.length >= MAX_SCREEN_LENTH) {
+
+    } else if (screen.textContent == 0 || numberLock) {
+        screen.textContent = this.id;
+        numberLock = false;
+    } else {
+        screen.textContent = screen.textContent + this.id;
+    }
+}
+
+const initialize = function() {
+
+    numberButtons.forEach((button) => {
+        button.addEventListener('click', displayText);
     });
-});
+
+    ac.addEventListener('click', clear);
+
+    resultButton.addEventListener('click', runResult);
+
+    operationButtons.forEach((button) => {
+        button.addEventListener('click', onOperatorButtonClick);
+    });
+}
+
+initialize();
